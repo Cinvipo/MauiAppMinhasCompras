503527e5ac11099de2cb26ee9a642448e93c9d93
cinti <cinti@LAPTOP-STF0TN22>
2025-08-17 12:08:58 -0300

    Contrucao da classe CRUD no SQLite.

--- MauiAppMinhasCompras/Helpers/SQliteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQliteDatabaseHelper.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using MauiAppMinhasCompras.Models;
+using System.Security.Cryptography.X509Certificates;
+
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQliteDatabaseHelper
+    public class SQliteDatabaseHelper
     {
+        private readonly SQLiteAsyncConnection _conn; // Fix for CS0103: Declare the _conn field
+
+        public SQliteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path); // Fix for CS1729: Correct the class name to SQLiteAsyncConnection
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>>Update(Produto p)
+
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+
+        }
+        
+                        
+            public Task<int> Delete(int id) 
+        {
+            return _conn.Table<Produto>().DeleteAsync (i => i.Id == id);
+        }
+
+            public Task<List<Produto>>GetAll() 
+        {
+                return _conn.Table<Produto>().ToListAsync(); 
+        }
+
+            public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+                return _conn.QueryAsync<Produto>(sql);
+;         }
+
+
+
     }
-}
+
+    }
+
+    
+
